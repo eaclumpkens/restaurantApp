11c5dc9583e609c2ac8a14dbd19cc11977efcfa4
eaclumpkens <eaclumpkens@gmail.com>
2020-11-18 00:00:40 -0500

    port issue

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
 
-var PORT = process.env.ORT || 8080;
+var PORT = process.env.PORT || 8080;
 var app = express();
 
 app.use(express.static("public"));
