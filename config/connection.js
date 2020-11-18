5c9768c5bae4ccfd7fc5c83c547f49c80d0d83cf
eaclumpkens <eaclumpkens@gmail.com>
2020-11-17 23:46:05 -0500

    test

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,11 @@
 var mysql = require("mysql");
-var connection;
 
+var connection;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
+    port: 3306,
     host: 'localhost',
     user: 'root',
     password: ' ',
